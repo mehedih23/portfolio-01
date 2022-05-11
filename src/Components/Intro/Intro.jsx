b25dc8640d880a74ad9b1cdf17dcf9fe7df8b142
mehedih23 <nmehedi23@gmail.com>
2022-05-11 12:23:20 +0600

    navbar done

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Intro.css';
+
+const Intro = () => {
+  return (
+    <div className='intro'>
+        <div className='i-left'>
+        <div className='i-name'>
+        <span>Hy! I Am</span>
+        <span>Mehedi Hassan</span>
+        <span>Frontend Developer with high level of experience in web designing and development, producting the quality work.</span>
+        </div>
+        <button className='button i-button'>Hire me</button>
+        </div>
+        <div className='i-right'>
+            i am right side
+        </div>
+    </div>
+  )
+}
+
+export default Intro
